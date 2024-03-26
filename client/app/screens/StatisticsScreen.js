efdf9bf70831bbf4575b30eaf720aaf8bb7ea7e9
Đức Anh <doducanh2411@gmail.com>
2024-03-26 23:31:09 +0700

    Make garden zoomable

--- client/app/screens/StatisticsScreen.js
+++ client/app/screens/StatisticsScreen.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
+import { LineChart, ProgressChart } from "react-native-chart-kit";
+import { Dimensions, Button } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
 
 export default function StatisticsScreen() {
+  const [showModal, setShowModal] = useState(false);
+
   const [map, setMap] = useState([
     [1, 0, 0, 0, 0, 0, 6],
     [1, 0, 0, 0, 0, 0, 3],
@@ -86,42 +93,109 @@ export default function StatisticsScreen() {
     ));
   };
 
+  const chartConfig = {
+    backgroundColor: "#e26a00",
+    backgroundGradientFrom: "#fff",
+    backgroundGradientTo: "#fff",
+    color: (opacity = 1) => `rgba(80, 170, 117, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
+  const data1 = {
+    labels: ["3/1", "6/3", "11/3", "16/3", "21/3", "26/3", "31/3"],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43, 20, 45, 28, 80, 99, 43],
+        strokeWidth: 2 // optional
+      }
+    ],
+    legend: ["😊"] // optional
+  };
+
+  const data2 = {
+    labels: ["Swim", "Bike", "Run"], // optional
+    data: [0.4, 0.6, 0.8]
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.mapContainer}>
-        {renderMap()}
+      <View style={styles.gardern}>
+        <View style={{ backgroundColor: 'transparent', height: 50, flexDirection: 'row', justifyContent: 'flex-end', paddingTop: 50, zIndex: 1 }}>
+          <View>
+            <TouchableOpacity activeOpacity={0.5} onPress={() => setShowModal(true)}>
+              <Image
+                source={require('../../assets/garden/box.png')}
+                style={styles.image}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <ImageBackground
+              source={require('../../assets/garden/move.png')}
+              style={styles.image}
+            />
+          </View>
+        </View>
+        <ReactNativeZoomableView
+          maxZoom={1.5}
+          minZoom={0.5}
+          zoomStep={0.5}
+          initialZoom={0.8}
+          bindToBorders={true}
+          onZoomAfter={this.logOutZoomState}
+          style={{}}
+        >
+          <View style={styles.mapContainer}>
+            {renderMap()}
+          </View>
+        </ReactNativeZoomableView>
+      </View>
+
+      <View style={{ flex: 3, backgroundColor: '#F4F2EE' }}>
+        <LineChart
+          data={data1}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          bezier
+        />
       </View>
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  gardern: {
+    flex: 3,
+    backgroundColor: '#d8e1d0',
+    width: '100%',
+    height: '100%'
   },
   mapContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
     // transform: [{ rotateX: '45deg' }, { rotateZ: '45deg' }],
-    transform: [{ rotateX: '45deg' }, { rotateY: '0deg' }, { rotateZ: '45deg' }]
+    transform: [{ rotateX: '20deg' }, { rotateY: '0deg' }, { rotateZ: '45deg' }],
+
   },
   row: {
     flexDirection: 'row',
   },
   cell: {
-    width: 45,
-    height: 45,
+    width: 35,
+    height: 35,
     margin: 0,
-    borderWidth: 1,
+    borderWidth: 0,
     borderColor: "#fff"
   },
   image: {
-    width: 46,
-    height: 46,
+    width: 36,
+    height: 36,
     resizeMode: 'contain',
     borderWidth: 0,
     borderColor: "#fff"
@@ -132,9 +206,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   tree: {
-    width: 50,
-    height: 50,
-    marginTop: -35,
-    marginLeft: -15,
+    width: 45,
+    height: 45,
+    marginTop: -20,
+    marginLeft: -5,
   }
 });
