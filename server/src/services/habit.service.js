54a8144c9556e90d89793c0644f571eca7268154
onlyzabao <ntgbao.235@gmail.com>
2024-04-08 20:51:19 +0700

    modify: create habit api

--- server/src/services/habit.service.js
+++ server/src/services/habit.service.js
@@ -12,14 +12,8 @@ class HabitService {
 
     find = async (conditions) =>
         Habit.findOne({
-            attributes: { exclude: ["createdAt", "updatedAt"] },
             where: conditions,
-            include: [
-                {
-                    association: Habit.Criteria,
-                    attributes: { exclude: ["createdAt", "updatedAt"] },
-                },
-            ],
+            include: [{ association: Habit.Criteria }],
         });
 
     update = async (instance, data) => instance.update(data);
