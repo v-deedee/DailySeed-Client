2efefbe9124d7450f043f08bfe3cc98bf5a07dea
onlyzabao <ntgbao.235@gmail.com>
2024-04-09 09:11:26 +0700

    refactor

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = process.env.PORT || 5050;
-const HOSTNAME = process.env.HOSTNAME || "192.168.110.59";
+const HOSTNAME = process.env.HOSTNAME || "localhost";
 
 const httpServer = http.createServer(app);
 httpServer.listen(PORT, HOSTNAME, () => {
